c9b5bbfc0cad18e0121c976e7327f5c00627b611
duong-thai-nhat <63115734+duong-thai-nhat@users.noreply.github.com>
2022-04-25 01:19:55 +0700

    fig UI v-6

--- script.js
+++ script.js
@@ -31,25 +31,25 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
-function showSlides1(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides1");
-  let dots = document.getElementsByClassName("dot");
+// function showSlides1(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides1");
+//   let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
+//   if (n > slides.length) {
+//     slideIndex = 1
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length
+//   }
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
 
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
